04b150adc1c40e74994fb252c3d99f1954477312
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-07-11 23:01:59 -0700

    models product fields and rules

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -2,11 +2,20 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
@@ -17,4 +26,4 @@ Category.init(
   }
 );
 
-module.exports = Category
+module.exports = Category;
