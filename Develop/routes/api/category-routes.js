d858ff8cbc847e2d495f04dd72612202b4822d9e
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-07-17 14:38:28 -0700

    post/ put category api setup

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -38,13 +38,40 @@ router.get('/:id', (req, res) => {
       });
   });
 
-router.post('/', (req, res) => {
-    // create a new category
-});
+  router.post('/', (req, res) => {
+    Category.create({
+      category_name: req.body.category_name
+    })
+      .then(categoryData => res.json(categoryData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
 
-router.put('/:id', (req, res) => {
-    // update a category by its `id` value
-});
+  router.put('/:id', (req, res) => {
+    Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(categoryData => {
+        if (!categoryData) {
+          res.status(404).json({ message: 'No Category found with that ID.' });
+          return;
+        }
+        res.json(categoryData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 router.delete('/:id', (req, res) => {
     // delete a category by its `id` value
